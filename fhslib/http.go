ac0b1715e28b2a7d8676fbcc884160ca2c483486
appcrash <cosmosgleam@gmail.com>
2018-11-11 06:53:25 +0000

    rewrite getrequests/getresponses with state machine

--- fhslib/http.go
+++ fhslib/http.go
@@ -7,16 +7,22 @@ import (
 	"io"
 	"net"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 const (
 	stateFirstLine = iota
 	stateFirstLineMore
+	stateFirstLineDone
 	stateHeader
-	stateDelimiter
+	stateHeaderMore
+	stateHeaderDone
 	stateBody
+	stateBodyMore
 )
+
+var delimiter []byte = []byte("\r\n")
 
 type Header map[string]string
 type Request struct {
@@ -161,134 +167,292 @@ func ParseHeaders(buf string) (Header, error) {
 }
 
 func GetRequests(reader io.Reader, c chan *Request) {
-	const bufsize = 1024 * 1024 * 4
-	rbuf := make([]byte, bufsize)
-	var n int
+	var n, body_left, body_length int
 	var err error
-	var line, more_line []byte
+	var line, more_line, body_data []byte
 	var has_more bool
+	var action, path string
+	var headers *Header
 	action_regex, _ := regexp.Compile(`(?i)(get|post)\s+(/[/\w]*)\s+http/(1.1|2)`)
-	delimiter_regex, _ := regexp.Compile(`\r\n\r\n`)
+	header_regex, _ := regexp.Compile(`([\w\-]+):\s*([\w:\-_*/]+)`)
 	rd := bufio.NewReader(reader)
 
 	state := stateFirstLine
+Loop:
 	for {
 		switch state {
 		case stateFirstLine:
 			if line, has_more, err = rd.ReadLine(); err != nil {
-				Log.Errorf("http get request when in state first line with error: %s", err)
-				break
+				if err != io.EOF {
+					Log.Errorf("GetRequests when in state first line with error: %s", err)
+				} else {
+					Log.Debug("GetRequests when in state first line with EOF")
+				}
+
+				break Loop
 			}
 			if has_more {
-				Log.Debug("http get request when first line has more(too big)")
+				Log.Debug("GetRequests when first line has more(too big)")
 				state = stateFirstLineMore
 				continue
 			}
-
+			state = stateFirstLineDone
 		case stateFirstLineMore:
 			if more_line, has_more, err = rd.ReadLine(); err != nil {
-				Log.Errorf("http get request when in state first more line wtih error: %s", err)
-				break
+				Log.Errorf("GetRequests when in state first more line wtih error: %s", err)
+				break Loop
 			}
 			line = append(line, more_line...)
 			if !has_more {
-				if indexs := action_regex.FindSubmatchIndex(line); indexs == nil {
-					Log.Errorf("GetRequests can not find action line, line: %s", line)
-				}
-
-				state = stateHeader
+				state = stateFirstLineDone
 			}
+		case stateFirstLineDone:
+			if indexs := action_regex.FindSubmatchIndex(line); indexs == nil {
+				Log.Errorf("GetRequests can not find action line, line: %s", line)
+				break Loop
+			} else {
+				action = strings.ToUpper(string(line[indexs[2]:indexs[3]]))
+				path = string(line[indexs[4]:indexs[5]])
+			}
+			state = stateHeader
+			headers = &Header{} // create new headers for this request
 		case stateHeader:
-		case stateDelimiter:
-		case stateBody:
-		}
-		n, err = reader.Read(rbuf)
-		if err != nil {
-			if err != io.EOF {
-				Log.Errorf("GetRequests with error %s", err)
+			if line, has_more, err = rd.ReadLine(); err != nil {
+				Log.Errorf("GetRequests when in state header line with error: %s", err)
+				break Loop
 			}
-			break
+			if has_more {
+				Log.Debug("GetRequests when header line has more(too big)")
+				state = stateHeaderMore
+				continue
+			}
+			Log.Debugf("GetRequests read header line %s", line)
+			state = stateHeaderDone
+		case stateHeaderMore:
+			if more_line, has_more, err = rd.ReadLine(); err != nil {
+				Log.Errorf("GetRequests when in state header more line wtih error: %s", err)
+				break Loop
+			}
+			line = append(line, more_line...)
+			if !has_more {
+				Log.Debugf("GetRequests read header line %s", line)
+				state = stateHeaderDone
+			}
+		case stateHeaderDone:
+			if indexs := header_regex.FindSubmatchIndex(line); indexs == nil {
+				Log.Errorf("GetRequests can not validate header line, line: %s", line)
+				break Loop
+			} else {
+				header_name := string(line[indexs[2]:indexs[3]])
+				header_value := string(line[indexs[4]:indexs[5]])
+				(*headers)[strings.ToUpper(header_name)] = header_value
+			}
+
+			if next, next_err := rd.Peek(2); next_err != nil {
+				Log.Errorf("GetRequests peek next 2 bytes at header end with error: %s", next_err)
+				break Loop
+			} else {
+				if bytes.Compare(next, delimiter) == 0 { // we found the delimiter after reading one header line, skip it to body
+					rd.Discard(2)
+					state = stateBody
+					continue
+				}
+			}
+			state = stateHeader
+
+		case stateBody:
+			switch action {
+			case "GET", "HEAD":
+				body_data = nil
+				body_left = 0
+			case "POST":
+				if length_str, exists := (*headers)["CONTENT-LENGTH"]; !exists {
+					Log.Errorf("GetRequests headers has no content-length: %v", headers)
+					break Loop
+				} else {
+					if body_length, err = strconv.Atoi(length_str); err != nil {
+						Log.Errorf("GetRequests parse content length(%s) error:%s", length_str, err)
+						break Loop
+					}
+				}
+				body_data = make([]byte, body_length)
+				body_left = body_length
+			}
+
+			state = stateBodyMore
+		case stateBodyMore:
+			for body_left > 0 {
+				if n, err = rd.Read(body_data[(body_length - body_left):]); err != nil {
+					break
+				} else {
+					body_left -= n
+				}
+			}
+
+			if err != nil {
+				if err != io.EOF {
+					Log.Errorf("GetRequests read body with error %s when body_left:%d, body_length:%d",
+						err, body_left, body_length)
+				} else {
+					Log.Debug("GetRequests got EOF")
+				}
+				// exit the read cycle
+				break Loop
+			}
+
+			data_buf := bytes.Buffer{}
+			if body_data != nil {
+				data_buf.Write(body_data)
+			}
+
+			request := Request{action, path, headers, &data_buf}
+			c <- &request
+
+			state = stateFirstLine // read next request
 		}
-
-		indexs := action_regex.FindSubmatchIndex(rbuf)
-		if indexs == nil {
-			Log.Error("GetRequests can not find action line")
-			break
-		}
-		action := string(rbuf[indexs[2]:indexs[3]])
-		path := string(rbuf[indexs[4]:indexs[5]])
-		rest := rbuf[indexs[1]:n]
-		// Log.Debugf("rest is %s", rest)
-
-		delimiter_index := delimiter_regex.FindIndex(rest)
-		if delimiter_index == nil {
-			Log.Errorf("GetRequests can not find delimiter")
-			break
-		}
-
-		header_end := delimiter_index[1] - 2
-		header, err := ParseHeaders(string(rest[:header_end]))
-		if err != nil {
-			Log.Errorf("header error %s", err)
-			Log.Error("GetRequests parse header error")
-			break
-		}
-
-		data_buf := bytes.Buffer{}
-		data_buf.Write(rest[delimiter_index[1]:])
-
-		request := Request{action, path, &header, &data_buf}
-		c <- &request
 	}
 
 	c <- nil
 }
 
 func GetResponses(reader io.Reader, c chan *Response) {
-	const bufsize = 128 * 1024
-	rbuf := make([]byte, bufsize)
-	var n int
+	var n, body_left, body_length int
 	var err error
-	state_regex, _ := regexp.Compile(`(?i)http/(1.1|2)\s+(\d\d\d)\s+[^\r\n]+\r\n`)
-	delimiter_regex, _ := regexp.Compile(`\r\n\r\n`)
+	var line, more_line, body_data []byte
+	var has_more bool
+	var state_code string
+	var headers *Header
+	state_line_regex, _ := regexp.Compile(`(?i)http/(1.1|2)\s+(\d\d\d)\s+[^\r\n]+`)
+	header_regex, _ := regexp.Compile(`([\w\-]+):\s*([\w:\-_*/]+)`)
+	rd := bufio.NewReader(reader)
 
+	state := stateFirstLine
+Loop:
 	for {
-		n, err = reader.Read(rbuf)
-		if err != nil {
-			if err != io.EOF {
-				Log.Errorf("response reader error with error:%s", err)
+		switch state {
+		case stateFirstLine:
+			if line, has_more, err = rd.ReadLine(); err != nil {
+				if err != io.EOF {
+					Log.Errorf("GetResponses when in state first line with error: %s", err)
+				} else {
+					Log.Debug("GetResponses when in state first line with EOF")
+				}
+
+				break Loop
 			}
-			break
+			if has_more {
+				Log.Debug("GetResponses when first line has more(too big)")
+				state = stateFirstLineMore
+				continue
+			}
+			state = stateFirstLineDone
+		case stateFirstLineMore:
+			if more_line, has_more, err = rd.ReadLine(); err != nil {
+				Log.Errorf("GetResponses when in state first more line wtih error: %s", err)
+				break Loop
+			}
+			line = append(line, more_line...)
+			if !has_more {
+				state = stateFirstLineDone
+			}
+		case stateFirstLineDone:
+			if indexs := state_line_regex.FindSubmatchIndex(line); indexs == nil {
+				Log.Errorf("GetResponses can not find state code line, line: %s", line)
+				break Loop
+			} else {
+				state_code = string(line[indexs[4]:indexs[5]])
+			}
+			state = stateHeader
+			headers = &Header{} // create new headers for this response
+		case stateHeader:
+			if line, has_more, err = rd.ReadLine(); err != nil {
+				Log.Errorf("GetResponses when in state header line with error: %s", err)
+				break Loop
+			}
+			if has_more {
+				Log.Debug("GetResponses when header line has more(too big)")
+				state = stateHeaderMore
+				continue
+			}
+			Log.Debugf("GetResponses read header line %s", line)
+			state = stateHeaderDone
+		case stateHeaderMore:
+			if more_line, has_more, err = rd.ReadLine(); err != nil {
+				Log.Errorf("GetResponses when in state header more line wtih error: %s", err)
+				break Loop
+			}
+			line = append(line, more_line...)
+			if !has_more {
+				Log.Debugf("GetResponses read header line %s", line)
+				state = stateHeaderDone
+			}
+		case stateHeaderDone:
+			if indexs := header_regex.FindSubmatchIndex(line); indexs == nil {
+				Log.Errorf("GetResponses can not validate header line, line: %s", line)
+				break Loop
+			} else {
+				header_name := string(line[indexs[2]:indexs[3]])
+				header_value := string(line[indexs[4]:indexs[5]])
+				(*headers)[strings.ToUpper(header_name)] = header_value
+			}
+
+			if next, next_err := rd.Peek(2); next_err != nil {
+				Log.Errorf("GetResponses peek next 2 bytes at header end with error: %s", next_err)
+				break Loop
+			} else {
+				if bytes.Compare(next, delimiter) == 0 { // we found the delimiter after reading one header line, skip it to body
+					rd.Discard(2)
+					state = stateBody
+					continue
+				}
+			}
+			state = stateHeader
+
+		case stateBody:
+			// content length is a must, currently not support chunk
+			if length_str, exists := (*headers)["CONTENT-LENGTH"]; !exists {
+				Log.Errorf("GetResponses headers has no content-length: %v", headers)
+				break Loop
+			} else {
+				if body_length, err = strconv.Atoi(length_str); err != nil {
+					Log.Errorf("GetResponses parse content length(%s) error:%s", length_str, err)
+					break Loop
+				}
+			}
+			body_data = make([]byte, body_length)
+			body_left = body_length
+
+			state = stateBodyMore
+		case stateBodyMore:
+			for body_left > 0 {
+				if n, err = rd.Read(body_data[(body_length - body_left):]); err != nil {
+					break
+				} else {
+					body_left -= n
+				}
+			}
+
+			if err != nil {
+				if err != io.EOF {
+					Log.Errorf("GetResponses read body with error %s when body_left:%d, body_length:%d",
+						err, body_left, body_length)
+				} else {
+					Log.Debug("GetResponses got EOF")
+				}
+				// exit the read cycle
+				break Loop
+			}
+
+			data_buf := bytes.Buffer{}
+			if body_data != nil {
+				data_buf.Write(body_data)
+			}
+
+			response := Response{state_code, headers, &data_buf}
+			c <- &response
+
+			state = stateFirstLine // read next response
 		}
-
-		indexs := state_regex.FindSubmatchIndex(rbuf)
-		if indexs == nil {
-			Log.Error("GetResponses can not find state line")
-			break
-		}
-
-		state := string(rbuf[indexs[4]:indexs[5]])
-		rest := rbuf[indexs[1]:n]
-
-		delimiter_index := delimiter_regex.FindIndex(rest)
-		if delimiter_index == nil {
-			Log.Error("GetRersponses can not find delimiter")
-			break
-		}
-
-		header_end := delimiter_index[1] - 2
-		header, err := ParseHeaders(string(rest[:header_end]))
-		if err != nil {
-			Log.Errorf("parse response header error %s", err)
-			break
-		}
-
-		data_buf := bytes.Buffer{}
-		data_buf.Write(rest[delimiter_index[1]:])
-
-		response := Response{state, &header, &data_buf}
-		c <- &response
-
 	}
 
 	c <- nil
--- fhslib/http_test.go
+++ fhslib/http_test.go
@@ -10,64 +10,75 @@ func init() {
 	SetLogLevel("debug")
 }
 
-func TestHeader(t *testing.T) {
-	buf := bytes.Buffer{}
-	AddHeader(&buf, "Content-Length", "80")
-	AddHeader(&buf, "Content-Type", "text/html")
-
-	headers, err := ParseHeaders(buf.String())
-	t.Logf("headers are %v", headers)
-	if err != nil {
-		t.Error(err)
-	}
-
-	v, ok := headers["CONTENT-LENGTH"]
-	if !ok {
-		t.Error("header name not found")
-	}
-	if v != "80" {
-		t.Error("header value not correct")
-	}
-}
-
 func TestParseRequest(t *testing.T) {
 	buf := bytes.Buffer{}
-	content := "post sample content"
+	content1 := "post sample content"
+	content2 := "post other data "
 
 	AddAction(&buf, "post", "/some/loc")
 	AddHeader(&buf, "content-type", "text/html")
+	AddHeader(&buf, "content-length", strconv.Itoa(len(content1)))
 	AddDelimiter(&buf)
-	AddData(&buf, []byte(content))
+	AddData(&buf, []byte(content1))
+
+	AddAction(&buf, "post", "/sdoiew")
+	AddHeader(&buf, "content-length", strconv.Itoa(len(content2)))
+	AddDelimiter(&buf)
+	AddData(&buf, []byte(content2))
+
+	AddAction(&buf, "get", "/")
+	AddHeader(&buf, "accept", "*/*")
+	AddDelimiter(&buf)
 
 	c := make(chan *Request)
 	go GetRequests(&buf, c)
-	req := <-c
+	print_req := func() {
+		req := <-c
+		t.Logf("request action is %s, path is %s", req.Action, req.Path)
+		t.Logf("headers are %v", req.Header)
+		t.Logf(req.Data.String())
+	}
 
-	t.Logf(req.Action)
-	t.Logf("headers are %v", req.Header)
-	t.Logf(req.Data.String())
+	for i := 0; i < 3; i++ {
+		print_req()
+	}
+
 }
 
 func TestParseResponse(t *testing.T) {
 	buf := bytes.Buffer{}
-	content := "some response sample content"
+	content1 := "some response sample content"
+	content2 := "another response sample content"
 
 	AddState(&buf, 200)
-	AddHeader(&buf, "content-type", "text/html")
-	AddHeader(&buf, "content-length", strconv.Itoa(len(content)))
+	AddHeader(&buf, "content-type", "image/jpg")
+	AddHeader(&buf, "content-length", strconv.Itoa(len(content1)))
 	AddDelimiter(&buf)
-	AddData(&buf, []byte(content))
+	AddData(&buf, []byte(content1))
+
+	AddState(&buf, 404)
+	AddHeader(&buf, "content-type", "text/html")
+	AddHeader(&buf, "content-length", strconv.Itoa(len(content2)))
+	AddDelimiter(&buf)
+	AddData(&buf, []byte(content2))
 
 	c := make(chan *Response)
 	go GetResponses(&buf, c)
-	resp := <-c
 
-	if resp == nil {
-		t.Error("parse response error")
-		return
+	print_resp := func() {
+		resp := <-c
+
+		if resp == nil {
+			t.Error("parse response error")
+			return
+		}
+		t.Logf("response state code is %s", resp.State)
+		t.Logf("headers are %v", resp.Header)
+		t.Logf(resp.Data.String())
 	}
 
-	t.Logf("response state code is %s", resp.State)
-	t.Logf("headers are %v", resp.Header)
-	t.Logf(resp.Data.String())
+	for i := 0; i < 2; i++ {
+		print_resp()
+	}
+
 }
